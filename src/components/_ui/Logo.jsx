e57d989b78aafa0f262984725f0ef1217725a5c1
piyush5103 <15842633+piyush5103@users.noreply.github.com>
2020-08-22 11:51:13 +0530

    Update Logo.jsx

--- src/components/_ui/Logo.jsx
+++ src/components/_ui/Logo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Logo = () => (
-    <img width="71" height="31" viewBox="0 0 71 31" src="https://ik.imagekit.io/piyush/logo_EIqRM5gS9.png" >
+    <img src={"./logo.png"} >
 );
 
 export default Logo;
