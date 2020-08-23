8a8a89559efce8673155b240fb65347906abd9b5
piyush5103 <15842633+piyush5103@users.noreply.github.com>
2020-08-24 00:50:17 +0530

    Create about.js

---
+++ src/pages/about.js
@@ -0,0 +1,151 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import { RichText } from "prismic-reactjs"
+import { graphql, Link } from "gatsby"
+import styled from "@emotion/styled"
+import colors from "styles/colors"
+import dimensions from "styles/dimensions"
+import Button from "components/_ui/Button"
+import About from "components/About"
+import Layout from "components/Layout"
+import ProjectCard from "components/ProjectCard"
+
+
+const AboutTitle = styled("h1")`
+    margin-bottom: 1em;
+`
+
+const Section = styled("div")`
+  margin-bottom: 10em;
+  display: flex;
+  flex-direction: column;
+  @media (max-width: ${dimensions.maxwidthTablet}px) {
+    margin-bottom: 4em;
+  }
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+`
+
+
+const RenderBody = ({ home, projects, meta }) => (
+  <>
+    <Helmet
+            title={`About | Prist, Gatsby & Prismic Starter`}
+            titleTemplate={`%s | About | Prist, Gatsby & Prismic Starter`}
+            meta={[
+                {
+                    name: `description`,
+                    content: meta.description,
+                },
+                {
+                    property: `og:title`,
+                    content: `About | Prist, Gatsby & Prismic Starter`,
+                },
+                {
+                    property: `og:description`,
+                    content: meta.description,
+                },
+                {
+                    property: `og:type`,
+                    content: `website`,
+                },
+                {
+                    name: `twitter:card`,
+                    content: `summary`,
+                },
+                {
+                    name: `twitter:creator`,
+                    content: meta.author,
+                },
+                {
+                    name: `twitter:title`,
+                    content: meta.title,
+                },
+                {
+                    name: `twitter:description`,
+                    content: meta.description,
+                },
+            ].concat(meta)}
+        />
+   
+   
+    <Layout>
+    <AboutTitle>
+                About
+            </AboutTitle>
+      {RichText.render(home.about_title)}
+      <About bio={home.about_bio} socialLinks={home.about_links} />
+    </Layout>
+  </>
+)
+
+export default ({ data }) => {
+  //Required check for no data being returned
+  const doc = data.prismic.allHomepages.edges.slice(0, 1).pop()
+  const projects = data.prismic.allProjects.edges
+  const meta = data.site.siteMetadata
+
+  if (!doc || !projects) return null
+
+  return (
+    <Layout>
+      <RenderBody home={doc.node} projects={projects} meta={meta} />
+    </Layout>
+  )
+}
+
+RenderBody.propTypes = {
+  home: PropTypes.object.isRequired,
+  projects: PropTypes.array.isRequired,
+  meta: PropTypes.object.isRequired,
+}
+
+export const query = graphql`
+  {
+    prismic {
+      allHomepages {
+        edges {
+          node {
+            hero_title
+            hero_button_text
+            hero_button_link {
+              ... on PRISMIC__ExternalLink {
+                _linkType
+                url
+              }
+            }
+            content
+            about_title
+            about_bio
+            about_links {
+              about_link
+            }
+          }
+        }
+      }
+      allProjects {
+        edges {
+          node {
+            project_title
+            project_preview_description
+            project_preview_thumbnail
+            project_category
+            project_post_date
+            _meta {
+              uid
+            }
+          }
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        title
+        description
+        author
+      }
+    }
+  }
+`
